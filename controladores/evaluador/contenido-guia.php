fe835fe17198ada5181cb652ce03fba98b050d8d
Ricardo Suárez <ur.suarez@gmail.com>
2017-09-23 21:12:18 -0500

    cambio contenido guia

--- controladores/evaluador/contenido-guia.php
+++ controladores/evaluador/contenido-guia.php
@@ -12,66 +12,58 @@
 
         <br/>
 
-        <div class="row grupo-botones-inline btn btn-lg btn-default" align="center">
-			<h4>Herramientas</h4>
-		</div>
-
-        <br/>
-
-        <div class="row grupo-botones-inline btn btn-lg btn-default" align="center">
-            <a data-toggle="modal" data-target="#myModal" href="" class="see-user">
-                <h4>Documentos</h4>
-            </a>
-		</div>
-
-        <br/>
-
-        <div class="row grupo-botones-inline btn btn-lg btn-default" align="center">
-			<h4>Guía de Implementación</h4>
+        <div class="row" align="center">
+			<div class="col-md-offset-4 col-md-4">
+                <a href="#" class="btn btn-lg btn-default btn-block">Herramientas</a>
+                <br><br>
+                <a href="#" class="btn btn-lg btn-default btn-block" data-toggle="modal" data-target="#myModal">Documentos</a>
+                <br><br>
+                <a href="#" class="btn btn-lg btn-default btn-block">Guía de Implementación</a>
+            </div>
 		</div>
 
 
         <div class="modal fade" id="myModal" role="dialog">
-        <div class="modal-dialog">
-    
-    
-        <!-- Modal content-->
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>          
-            </div>
-            <div class="modal-body">
-                <div class="row" align="left">
-                    <div class="col-md-6-offset-2">
-                        <p>
-                            A <B>paragraph</B> of text with on
-                            <u>unassigned link</u>
-                            A second row of text with a <a href="http://www.google.com"><u>web link</u></a>
-                        </p>
-                        <br/>
-                        <p>
-                            A <B>paragraph</B> of text with on
-                            <u>unassigned link</u>
-                            A second row of text with a <a href="http://www.google.com"><u>web link</u></a>
-                        </p>
-                        <br/>
-                        <p>
-                            A <B>paragraph</B> of text with on
-                            <u>unassigned link</u>
-                            A second row of text with a <a href="http://www.google.com"><u>web link</u></a>
-                        </p>
-                        <br/>
-                        <p>
-                            A <B>paragraph</B> of text with on
-                            <u>unassigned link</u>
-                            A second row of text with a <a href="http://www.google.com"><u>web link</u></a>
-                        </p>
+            <div class="modal-dialog">    
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>          
                     </div>
-                
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p>
+                                    A <B>paragraph</B> of text with on
+                                    <u>unassigned link</u>
+                                    A second row of text with a <a href="http://www.google.com"><u>web link</u></a>
+                                </p>
+                                <br/>
+                                <p>
+                                    A <B>paragraph</B> of text with on
+                                    <u>unassigned link</u>
+                                    A second row of text with a <a href="http://www.google.com"><u>web link</u></a>
+                                </p>
+                                <br/>
+                                <p>
+                                    A <B>paragraph</B> of text with on
+                                    <u>unassigned link</u>
+                                    A second row of text with a <a href="http://www.google.com"><u>web link</u></a>
+                                </p>
+                                <br/>
+                                <p>
+                                    A <B>paragraph</B> of text with on
+                                    <u>unassigned link</u>
+                                    A second row of text with a <a href="http://www.google.com"><u>web link</u></a>
+                                </p>
+                            </div>
+                            <div class="col-md-6">
+                                <img src="http://via.placeholder.com/500?text=imgPreview" class="img-preview" alt="">
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
             </div>
-        
         </div>
 
 <!-- Fin del contenido -->
--- controladores/evaluador/planes-accion.php
+++ controladores/evaluador/planes-accion.php
@@ -37,129 +37,130 @@
 	</div>
 
 	<div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Selecciona una pregunta</h4>
-        </div>
-        <div class="modal-body">
-          <div class="row" align="center">
-			<div class="col-md-8 col-md-offset-2">
-				<table class="table table-bordered table-striped">
-					<tbody>
-						<tr>
-							<td>
-								<a href="google.com" target="_blank" class="see-user">1 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">2 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">3 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">4 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">5 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">6 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">7 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">8 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">9 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">10 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">11 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">12 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">13 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">14 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">15 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">16 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">17 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">18 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">19 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="" class="see-user">20 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
-							</td>
-						</tr>
+	    <div class="modal-dialog">
+	    
+	      <!-- Modal content-->
+	      <div class="modal-content">
+	        <div class="modal-header">
+	          <button type="button" class="close" data-dismiss="modal">&times;</button>
+	          <h4 class="modal-title">Selecciona una pregunta</h4>
+	        </div>
 
-					</tbody>
-				</table>
-			</div>
-		</div>
-        </div>
-      </div>
-      
-    </div>
-  </div>
+	        <div class="modal-body">
+	          	<div class="row" align="center">
+					<div class="col-md-8 col-md-offset-2">
+						<table class="table table-bordered table-striped">
+							<tbody>
+								<tr>
+									<td>
+										<a href="google.com" target="_blank" class="see-user">1 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">2 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">3 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">4 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">5 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">6 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">7 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">8 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">9 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">10 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">11 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">12 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">13 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">14 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">15 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">16 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">17 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">18 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">19 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<a href="" class="see-user">20 - Lorem Ipsun Dolor Sit Amet <i class="glyphicon glyphicon-chevron-right icon-option"></i></a>
+									</td>
+								</tr>
+
+							</tbody>
+						</table>
+					</div>
+				</div>
+	        </div>
+	      </div>
+	      
+	    </div>
+ 	</div>
 	
 
   <!-- Fin contenido -->
