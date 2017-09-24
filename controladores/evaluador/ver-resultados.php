84dde2ade1d72322528ba981a8535d8729b01a6f
Ricardo Suárez <ur.suarez@gmail.com>
2017-09-24 00:25:39 -0500

    comb

--- controladores/evaluador/ver-resultados.php
+++ controladores/evaluador/ver-resultados.php
@@ -48,15 +48,14 @@
 
 		<div class="modal fade" id="Modal_NewUser" role="dialog">
 		    <div class="modal-dialog">
-		    
 		      	<!-- Modal content-->
 		      	<div class="modal-content">
 			        <div class="modal-header">
-			          <button type="button" class="close" data-dismiss="modal">&times;</button>
-			          <h4 class="modal-title modal_texto modal_textp">Nuevo Usuario</h4>
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h4 class="modal-title modal_texto modal_textp">Nuevo Usuario</h4>
 			        </div>
+			        
 			        <div class="modal-body modal_textp">
-
 			        	<form action="" method="">
 		          			<div class="row">
 		            			<div class="col-md-6 modal_texto">
@@ -74,9 +73,8 @@
 
 	    							<label for="observaciones">Observaciones</label>
     								<textarea class="form-control" id="obv_NewUser"></textarea>
-
-
 		            			</div>
+
 		            			<div class="col-md-6 modal_texto">
 		              				<label for="apellidos">Apellidos</label>
 	    							<input type="text" class="form-control" id="apellido_NewUser">
@@ -102,7 +100,7 @@
 										<div class="col-md-1">
 										</div>
 										<div class="col-md-4">
-											<button type="submit" class="btn btn-primary modal_boton_crear modal_boton_altura">Crear Usuario</button>
+											<button type="submit" class="btn btn-default modal_boton_crear modal_boton_altura">Crear Usuario</button>
 										</div>
 									</div>
 
@@ -114,22 +112,17 @@
 											<input type="file" id="foto_newUserDos">
 										</div>
 										<br>
-										<button type="submit" class="btn btn-primary">Crear Usuario</button>
+										<button type="submit" class="btn btn-default">Crear Usuario</button>
 										</div>
 									</div>
-
 		            			</div>
 		          			</div>
 		          		</form>
-
 	    			</div>
 		        </div>
 		        <!-- Final Modal-->
-
-		      </div>
-		      
 		    </div>
-		 </div>
+		</div>
 		
 	</div>
 	
